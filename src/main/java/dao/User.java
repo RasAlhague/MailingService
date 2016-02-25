618c1de3f98669fa1db48970cc3bbd33f508678e
RasAlhague <alexgr.dev@gmail.com>
2016-02-25 12:34:31 +0200

    Minimal impl

---
+++ src/main/java/dao/User.java
@@ -0,0 +1,22 @@
+package dao;
+
+public class User {
+    private String name;
+    private Boolean marked;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getMarked() {
+        return marked;
+    }
+
+    public void setMarked(Boolean marked) {
+        this.marked = marked;
+    }
+}
---
+++ src/main/java/services/mailsender/ParallelMailingService.java
@@ -0,0 +1,24 @@
+package services.mailsender;
+
+import dao.User;
+import dao.UserStorage;
+import services.MailingService;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ParallelMailingService implements MailingService {
+    private UserStorage userStorage;
+    private ExecutorService executor;
+
+    public ParallelMailingService(UserStorage userStorage, int threadPoolSize) {
+        this.userStorage = userStorage;
+        this.executor = Executors.newFixedThreadPool(threadPoolSize);
+    }
+
+    @Override
+    public void notifyMarkedUsers() {
+        List<User> markedUsers = userStorage.getMarked();
+    }
+}
