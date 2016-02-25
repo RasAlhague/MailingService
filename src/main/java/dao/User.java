6fe435ed0d481dfd2d3d2e4903770092d8c4f4f0
RasAlhague <alexgr.dev@gmail.com>
2016-02-25 13:07:29 +0200

    Upd user

--- src/main/java/dao/User.java
+++ src/main/java/dao/User.java
@@ -4,6 +4,14 @@ public class User {
     private String name;
     private Boolean marked;
 
+    public User() {
+    }
+
+    public User(String name, Boolean marked) {
+        this.name = name;
+        this.marked = marked;
+    }
+
     public String getName() {
         return name;
     }
@@ -18,5 +26,33 @@ public class User {
 
     public void setMarked(Boolean marked) {
         this.marked = marked;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", marked=").append(marked);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        return marked != null ? marked.equals(user.marked) : user.marked == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (marked != null ? marked.hashCode() : 0);
+        return result;
     }
 }
